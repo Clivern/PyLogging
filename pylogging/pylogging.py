0ab13e17025dbc0b85a975dcb14eb5f588d91c90
clivern <hello@clivern.com>
2015-08-27 22:44:32 +0200

    text storage type added

--- pylogging/pylogging.py
+++ pylogging/pylogging.py
@@ -5,88 +5,89 @@ from . import storage
 from . import mailer
 
 class PyLogging(dict, storage):
-	""" A custom logger class """
-	
-	LOG_FILE_FORMAT = "YYYY-MM-DD"
-	LOG_MESSAGE_FORMAT = "{DATE} {TYPE} {MESSAGE}"
-	DEFAULT_TYPE = "INFO"
-	STORAGE_TYPE = "text"
-	DATES_FORMAT = "YYYY-MM-DD"
-	DATETIME_FORMAT = "YYYY-MM-DD HH:MM:SS"
-	DEFAULT_TIMEZONE = "America/New York"
-	ALERT_STATUS = False
-	ALERT_EMAIL = "hello@example.com"
-	ALERT_TYPES = ['critical', 'error']
+    """ A custom logger class """
+    
+    LOG_FILE_FORMAT = 'YYYY-MM-DD'
+    LOG_MESSAGE_FORMAT = '{DATE} {TYPE} {MESSAGE}'
+    STORAGE_TYPE = 'text'
+    DATES_FORMAT = 'YYYY-MM-DD'
+    DATETIME_FORMAT = 'YYYY-MM-DD HH:MM:SS'
+    DEFAULT_TIMEZONE = 'America/New York'
+    ALERT_STATUS = False
+    ALERT_EMAIL = 'hello@example.com'
+    ALERT_TYPES = ['critical', 'error']
 
-	def __init__(self, **kargs):
-		self.logs_path = logs_path
-		
-	def __getattr__(self, name, default = False):
-		""" Get attributes """
-		if name in self.__dict__:
-			return self.__dict__[name]
-		elif name in self:
-			return self.get(name)
-		else:
-			# Check for denormalized name
-			name = self._denormalize(name)
-			if name in self:
-				return self.get(name)
-			else:
-				return default
+    def __init__(self, **kargs):
+        self.logs_path = logs_path
+        
+    def _updateConfigs(self, **kargs):
 
-
-	def __setattr__(self, name, value):
-		""" Set attributes """
-		if name in self.__dict__:
-			self.__dict__[name] = value
-		elif name in self:
-			self[name] = value
-		else:
-			# Check for denormalized name
-			name2 = self._normalize(name)
-			if name2 in self:
-				self[name2] = value
-			else:
-				# New attribute
-				self[name] = value
+    def __getattr__(self, name, default=False):
+        """ Get attributes """
+        if name in self.__dict__:
+            return self.__dict__[name]
+        elif name in self:
+            return self.get(name)
+        else:
+            # Check for denormalized name
+            name = self._denormalize(name)
+            if name in self:
+                return self.get(name)
+            else:
+                return default
 
 
-	def _normalize(self, value):
-		""" Normalize a string """
-
-		if value.find('-') != -1:
-			value = value.replace('-', '_')
-
-		return value
-
-
-	def _denormalize(self, value):
-		""" De-normalize a string """
-
-		if value.find('_') != -1:
-			value = value.replace('_', '-')
-
-		return value
+    def __setattr__(self, name, value):
+        """ Set attributes """
+        if name in self.__dict__:
+            self.__dict__[name] = value
+        elif name in self:
+            self[name] = value
+        else:
+            # Check for denormalized name
+            name2 = self._normalize(name)
+            if name2 in self:
+                self[name2] = value
+            else:
+                # New attribute
+                self[name] = value
 
 
-	def addFilter(self, filter, types = ['info', 'warning', 'error', 'critical', 'log']):
-		pass
+    def _normalize(self, value):
+        """ Normalize a string """
 
-	def removeFilter(self, filter):
-		pass	
+        if value.find('-') != -1:
+            value = value.replace('-', '_')
 
-	def info(self, msg):
-		pass
+        return value
 
-	def warning(self, msg):
-		pass
 
-	def error(self, msg):
-		pass
+    def _denormalize(self, value):
+        """ De-normalize a string """
 
-	def critical(self, msg):
-		pass
+        if value.find('_') != -1:
+            value = value.replace('_', '-')
 
-	def log(self, msg):
-		pass
+        return value
+
+
+    def addFilter(self, filter, types = ['info', 'warning', 'error', 'critical', 'log']):
+        pass
+
+    def removeFilter(self, filter):
+        pass    
+
+    def info(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        pass
+
+    def critical(self, msg):
+        pass
+
+    def log(self, msg):
+        pass
--- pylogging/storage.py
+++ pylogging/storage.py
@@ -1,45 +1,37 @@
 
-
-class JsonStorage():
-
-	def save(self):
-		""" Save message to .json file """
-		return True
-
 class TextStorage():
+    """ Text Storage Class """
+    
+    def write(self, log_file, msg):
+        """ Write message to .txt file """
+        with open(log_file, 'a') as LogFile:
+            LogFile.write(msg + os.linesep)
 
-	def save(self):
-		""" Save message to .text file """
-		return True
-
-
-class YamlStorage():
-
-	def save(self):
-		""" Save message to .yaml file """
-		return True
-
-class IniStorage():
-
-	def save(self):
-		""" Save message to .ini file """
-		return True
+    def read(self, log_file):
+        """ Read messages from .txt file """
+        if os.path.isfile(log_file):
+            with open(log_file, 'r') as LogFile:
+                data = LogFile.readlines()
+                data = "".join(line for line in data)
+        else:
+            data = ''
+        return data
 
 class Storage():
+    """ PyLogger Storage Class """
 
-	def __init__(self, log_file):
-		""" Init storage class with requested storage type """
-		if log_file.endswith('.json'):
-			self.storage = JsonStorage()
-		elif log_file.endswith('.text'):
-			self.storage = TextStorage()
-		elif log_file.endswith('.yaml'):
-			self.storage = YamlStorage()
-		elif log_file.endswith('.ini'):
-			self.storage = IniStorage()
-		else:
-			 raise Exception('Error Configuring PyLogger.Storage Class')
+    def __init__(self, log_file):
+        """ Init storage class wth requested storage type """
+        self.LOG_FILE = log_file
+        if self.LOG_FILE.endswith('.txt'):
+            self.storage = TextStorage()
+        else:
+            raise Exception('Error Configuring PyLogger.Storage Class')
 
-	def store(self, msg):
-		""" Store message in requested storage type """
-		return self.storage.save(msg)	
+    def write(self, msg):
+        """ Store message in requested storage type """
+        return self.storage.write(self.LOG_FILE, msg)
+
+    def read(self):
+        """ Read messages from requested storage type """
+        return self.storage.read(self.LOG_FILE)
