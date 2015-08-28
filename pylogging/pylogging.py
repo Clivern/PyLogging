5f000a577fc1921a61a0a7010925e02cbda004a2
clivern <hello@clivern.com>
2015-08-28 22:44:57 +0200

    actions feature added :+1:

--- pylogging/pylogging.py
+++ pylogging/pylogging.py
@@ -12,10 +12,13 @@ class PyLogging(dict):
     DATES_FORMAT = 'YYYY-MM-DD'
     DATETIME_FORMAT = 'YYYY-MM-DD HH:MM:SS'
     DEFAULT_TIMEZONE = 'America/New York'
+    
     ALERT_STATUS = False
     ALERT_EMAIL = 'hello@example.com'
     ALERT_TYPES = ['critical', 'error']
+
     FILTERS = []
+    ACTIONS = []
 
     def __init__(self, **kargs):
         self._config(**kargs)
@@ -38,28 +41,46 @@ class PyLogging(dict):
         self.FILTERS.append(filter)
         return "FILTER#{}".format(len(self.FILTERS) - 1)
 
+    def addAction(self, action):
+        self.ACTIONS.append(action)
+        return "ACTION#{}".format(len(self.ACTIONS) - 1)
+
     def _execFilters(self, type, msg):
         for filter in self.FILTERS:
             msg = filter(type, msg)
         return msg
+
+    def _execActions(self, type, msg):
+        for action in self.ACTIONS:
+            action(type, msg)
 
     def removeFilter(self, filter):
         filter = filter.split('#')
         del self.FILTERS[filter[1]]
         return True
 
+    def removeAction(self, action):
+        action = action.split('#')
+        del self.ACTIONS[action[1]]
+        return True
+
     def info(self, msg):
+        self._execActions('info', msg)
         msg = self._execFilters('info', msg)
         return msg
 
     def warning(self, msg):
+        self._execActions('warning', msg)
         msg = self._execFilters('warning', msg)
 
     def error(self, msg):
+        self._execActions('error', msg)
         msg = self._execFilters('error', msg)
 
     def critical(self, msg):
+        self._execActions('critical', msg)
         msg = self._execFilters('critical', msg)
 
     def log(self, msg):
+        self._execActions('log', msg)
         msg = self._execFilters('log', msg)
